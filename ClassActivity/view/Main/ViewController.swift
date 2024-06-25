a71aced3c6f8da9a07f9d10ed965c108e32f86e6
BrandoWeaver <srunrotha614@gmail.com>
2024-06-25 23:07:29 +0700

    on bottom brach

--- ClassActivity/view/Main/ViewController.swift
+++ ClassActivity/view/Main/ViewController.swift
@@ -1,11 +1,11 @@
 import UIKit
-
+//bottomBar branch
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        //bottomBar branch
+        
     }
 }
 
